cca9958077e869f25bc4ede4bc85428a6a58d3e2
Anthony Diaz <adiaz@loc.gov>
2023-11-17 12:27:22 -0500

    Correcting allowed origins from list to string

--- app/config/config.py
+++ app/config/config.py
@@ -10,7 +10,9 @@ class Settings(BaseSettings):
     APP_NAME: str = "FETCH"
     APP_ENVIRONMENT: str = "local"
     TIMEZONE: str = "America/New_York"
-    DATABASE_URL: str = "postgresql://postgres:postgres@host.docker.internal:5432/inventory_service"
+    DATABASE_URL: str = (
+        "postgresql://postgres:postgres@host.docker.internal:5432/inventory_service"
+    )
     # migration_url = database_url except in local
     MIGRATION_URL: str = (
         "postgresql://postgres:postgres@localhost:5432/inventory_service"
@@ -18,12 +20,7 @@ class Settings(BaseSettings):
     ENABLE_ORM_SQL_LOGGING: bool = True
     # Allowed origins for CORS
     ALLOWED_ORIGINS_REGEX: str = "https://*\.loctest\.gov, http://*\.loctest\.gov"
-    ALLOWED_ORIGINS: List[str] = [
-        "http://127.0.0.1:8080",
-        "https://127.0.0.1:8080",
-        "http://localhost:8080",
-        "https://localhost:8080",
-    ]
+    ALLOWED_ORIGINS: str = "http://127.0.0.1:8080,https://127.0.0.1:8080,http://localhost:8080,https://localhost:8080"
 
     class Config:
         env_file = ".env"
@@ -33,8 +30,7 @@ class Settings(BaseSettings):
 def get_settings() -> Settings:
     settings = Settings()
 
-    if isinstance(type(settings.ALLOWED_ORIGINS), str):
-        settings.ALLOWED_ORIGINS = settings.ALLOWED_ORIGINS.split(",")
+    settings.ALLOWED_ORIGINS = settings.ALLOWED_ORIGINS.split(",")
 
     print(f"Loading settings for {settings.APP_ENVIRONMENT}")
     return settings
